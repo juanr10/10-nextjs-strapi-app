edcd41ecb570939cfcd8d17b0a38d54c059147c1
juanr10 <argudojuan7@gmail.com>
2022-06-19 00:39:12 +0200

    create aboutUs component

---
+++ pages/aboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
+const AboutUs = () => {
+  return (
+    <Layout pageName="About us">
+      <h4 className="heading">About us</h4>
+    </Layout>
+  );
+};
+
+export default AboutUs;
