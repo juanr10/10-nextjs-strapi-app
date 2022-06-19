f5d2326fa85225f06569698685d4de74af462753
juanr10 <argudojuan7@gmail.com>
2022-06-19 20:50:44 +0200

    add date helper

---
+++ helpers/index.js
@@ -0,0 +1,11 @@
+export const dateFormatter = date => {
+    const newDate = new Date(date);
+
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit'
+    }
+
+    return newDate.toLocaleDateString('en-EN', options)
+}
